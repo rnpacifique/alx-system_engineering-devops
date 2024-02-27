8c052b51698d4ddee9625e3308726e90911eca39
rnpacifique <rpacfique12@gmail.com>
2024-02-27 10:03:50 +0200

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/bin/bash/pup
+# this is the flask installer
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
 
-# Instalation of a version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'werkzeug':
+    ensure   => '0.16.1',
+    provider => 'pip3',
+}
