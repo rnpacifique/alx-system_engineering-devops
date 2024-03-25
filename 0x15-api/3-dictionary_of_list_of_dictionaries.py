ea201d0c1a4118938191da8f7c28f85a26ae9a05
rnpacifique <rpacfique12@gmail.com>
2024-03-25 22:36:34 +0200

    task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,14 @@
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/")
+    users = user.json()
+    
+    filename= "todo_all_employees.json"
+    
+    for user in users:
+        json.dump
