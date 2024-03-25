9dcb9e3ea8e19473fadc9e44727616189239d3ac
rnpacifique <rpacfique12@gmail.com>
2024-03-25 22:29:03 +0200

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Python script that returns information about a given employee ID"""
-import re
+"""this for requesting a fake api"""
 import requests
-import sys
+from sys import argv
 
-"""The API's URL"""
-API_URL = 'https://jsonplaceholder.typicode.com'
-
-"""Check if the script is being run directly"""
 if __name__ == '__main__':
-    """Checks if command line arguments were provided"""
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            """Gets the user's information from the API"""
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
+    """to prvent to work when imported"""
 
-            """Filter todos for the given user"""
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
+    employee_id = argv[1]
+    users = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todos = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
 
-            """Print the user's tasks"""
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    user_data = users.json()
+
+    tasks_completed = 0
+    total_tasks = len(todos.json())
+
+    for task in todos.json():
+        if task['completed']:
+            tasks_completed += 1
+
+    print(
+        f"Employee {user_data['name']} is done with tasks"
+        f"({tasks_completed}/{total_tasks}):")
+
+    for task in todos.json():
+        if task['completed']:
+            tasks_completed += 1
+            print(f"\t {task['title']}")
