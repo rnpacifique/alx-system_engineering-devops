0dae55add0ee7b4e6eb839d3e1a7e0802f564456
rnpacifique <rpacfique12@gmail.com>
2024-04-09 12:21:39 +0200

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,13 @@
 #!/usr/bin/python3
-'''A function that queries Reddit API and returns the number of subscribers'''
+"""this is the module that get subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-    A function that queries Reddit API and returns the number of subscribers
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36'
-        ])
-    }
 
-    '''Make the API request'''
-    REDDIT_BASE_URL = 'https://www.reddit.com'
-    response = requests.get(
-            '{}/r/{}/about/.json'.format(REDDIT_BASE_URL, subreddit),
-            headers=api_headers,
-            allow_redirects=False
-    )
-
-    '''Check if the request was successful (status code 200)'''
-    if response.status_code == 200:
-        '''Parse the JSON response and extract the number of subscribers'''
-        subscribers = response.json()['data']['subscribers']
-        return subscribers
-
-    '''If the subreddit is invalid or the request fails, return 0'''
-    return 0
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
+    return response.json().get('data', {}).get('subscribers', 0)
